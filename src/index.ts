fe07aa223ce1e6b3a36bba57dcce830bfda0846e
ErrolKeith <errol.k.keith@gmail.com>
2025-10-06 18:23:50 -0400

    export all

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
 import { useBrowserStorage } from "./useBrowserStorage";
 import { makeOptionallyPrefixedKey } from "./lib/storage-key/key";
+import {
+  getLocalStorageItem,
+  removeLocalStorageItem,
+  setLocalStorageItem,
+} from "./lib/local-storage/operations";
+import {
+  getSessionStorageItem,
+  removeSessionStorageItem,
+  setSessionStorageItem,
+} from "./lib/session-storage/operations";
+import { getCookie, removeCookie, setCookie } from "./lib/cookies/operations";
 import type { BrowserStorage } from "./types";
 
-export { makeOptionallyPrefixedKey, useBrowserStorage };
+export {
+  getCookie,
+  getLocalStorageItem,
+  getSessionStorageItem,
+  makeOptionallyPrefixedKey,
+  removeCookie,
+  removeLocalStorageItem,
+  removeSessionStorageItem,
+  setCookie,
+  setLocalStorageItem,
+  setSessionStorageItem,
+  useBrowserStorage,
+};
 export type { BrowserStorage };
