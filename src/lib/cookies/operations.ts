32ec9f1bfe1bfffd0ee1f7eb9a529010cbe1f20d
ErrolKeith <errol.k.keith@gmail.com>
2025-10-07 16:15:33 -0400

    fn comments

--- src/lib/cookies/operations.ts
+++ src/lib/cookies/operations.ts
@@ -1,9 +1,21 @@
 import Cookie, { type CookieAttributes } from "js-cookie"
 
+/**
+ * get cookie by key
+ * 
+ * @param key 
+ * @returns 
+ */
 export const getCookie = (key: string) => {
   return Cookie.get(key);
 };
 
+/**
+ * set cookie
+ * @param key 
+ * @param value 
+ * @param cookieOptions 
+ */
 export const setCookie = (
   key: string,
   value: string,
@@ -12,6 +24,10 @@ export const setCookie = (
   Cookie.set(key, value, cookieOptions);
 };
 
+/**
+ * remove cookie by key
+ * @param key 
+ */
 export const removeCookie = (key: string) => {
   Cookie.remove(key);
 };
